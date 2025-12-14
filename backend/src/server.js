52963bc0ba71c279457c60e0045f6e8e4f9f1b29
sun-chingwoo <2005chinmayvarma@gmail.com>
2025-12-14 22:23:49 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,7 @@ import cors from "cors"
 dotenv.config()
 const app=express()
 
-app.use(cors(
-    {origin:"https://frnd-sepia.vercel.app"}
-))
+app.use(cors())
 app.use(express.json())
 connectDB()
 
